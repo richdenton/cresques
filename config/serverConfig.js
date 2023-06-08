d024507e1d87357a32c4340d7763392927f96d14
Rich Denton <me@richdenton.com>
2023-06-07 20:59:51 -0400

    Added email, password, and player name validation.

--- config/serverConfig.js
+++ config/serverConfig.js
@@ -1,7 +1,11 @@
 module.exports = {
+	emailMinLength: 5,
+	emailMaxLength: 100,
 	passwordMinLength: 8,
 	passwordMaxLength: 16,
 	passwordSaltLength: 8,
+	nameMinLength: 5,
+	nameMaxLength: 100,
 	tokenExpiration: 86400,
 	tokenSecret: process.env.TOKEN_SECRET
 };
